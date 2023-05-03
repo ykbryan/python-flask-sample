02c886ea938df7be89832526fc7cf754a4d83504
Bryan Chua <me@bryanchua.com>
2023-05-03 11:32:07 +0800

    update Flask

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,9 @@
-FROM python:3.7
+FROM python:3.8
 RUN mkdir /code
 RUN mkdir /config
 COPY ./ /code/
 WORKDIR /code
 COPY ./requirements.txt /config/
+EXPOSE 8080
+RUN pip install --upgrade pip
 RUN pip install -r /config/requirements.txt
--- application.py
+++ application.py
@@ -1,18 +1,25 @@
 #!/usr/bin/env python3
-from flask import Flask
+from flask import Flask, jsonify, request
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
     return 'Hello World! Demo!'
 
-@app.route('/fargate')
-def hello_fargate():
-    return 'Hello Fargate!'
 
-@app.route('/ec2')
-def hello_ec2():
-    return 'Hello ECS on EC2!'
+@app.route('/api/messages', methods=['GET'])
+def messages():
+    if (request.method == 'GET'):
+        return jsonify({'message': 'Hello World!'})
+    return jsonify({'message': 'You need to GET correctly!'})
+
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    return 'You want path: %s' % path
+
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0',port=8080)
+    app.run(debug=True, host='0.0.0.0', port=8080)
